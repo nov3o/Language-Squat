cafc58acaecfe7dda2df09a445fc129414027cdf
nov3o <ilya.badaneen@gmail.com>
2024-03-29 02:07:02 +0100

    styles, html, data

---
+++ engine.js
@@ -0,0 +1,9 @@
+/*
+  levels:       Dictionary(String, Array[String(3)])
+  allScripting: Array[String(3)]
+  allSpeech:    Array[String(3)]
+  speechCodes:  Dictionary(String(3), String)
+  scriptCodes:  Dictionary(String(3), String)
+  audiofiles:   Dictionary(String(3), Array[String](5))
+*/
+
